15ad0ec97b9b525a8a92e9ba0427bdd8269cb613
Thomas Aglassinger <roskakori@users.sourceforge.net>
2020-04-18 18:30:10 +0200

    #22 Added test case.

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -26,3 +26,24 @@ def test_can_postgres_bulk_load_tsv():
         with open(source_tsv_path, "rb") as source_tsv_file:
             with PostgresBulkLoad(database.engine) as bulk_load:
                 bulk_load.load(target_table, source_tsv_file)
+        with database.connection() as connection:
+            database.check_table_has_data(connection, target_table)
+
+
+@pytest.mark.skipif(
+    not IS_POSTGRES_DEFAULT_TEST_ENGINE,
+    reason="environment variable PIMDB_TEST_DATABASE must be set to postgres engine",
+)
+def test_fails_on_postgres_bulk_load_tsv_with_duplicate():
+    database = create_database_with_tables(DEFAULT_TEST_ENGINE)
+    dataset_to_load = ImdbDataset.NAME_BASICS
+    target_table = database.imdb_dataset_to_table_map[dataset_to_load]
+    source_tsv_path = os.path.join(
+        TESTS_DATA_PATH, test_fails_on_postgres_bulk_load_tsv_with_duplicate.__name__, dataset_to_load.tsv_filename
+    )
+    with pytest.raises(Exception, match=".*duplicate key.*"):
+        with open(source_tsv_path, "rb") as source_tsv_file:
+            with PostgresBulkLoad(database.engine) as bulk_load:
+                bulk_load.load(target_table, source_tsv_file)
+        with database.connection() as connection:
+            database.check_table_has_data(connection, target_table)
